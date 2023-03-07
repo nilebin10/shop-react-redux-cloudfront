679b166f8cbac0912e50b246428d4aa47bc4be5a
“Nilesh <nilesh_bind@epam.com>
2023-03-07 16:00:01 +0530

    [TASK-4] Get data from db

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-east-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-east-1.amazonaws.com/dev",
-  bff: "https://maihbqw7ji.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://tacex7h7eh.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-east-1.amazonaws.com/dev",
 };
 
