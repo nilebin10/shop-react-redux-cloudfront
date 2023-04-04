503f3e39c93baedb183c7c5bc5ea54f06d3705e6
“Nilesh <nilesh_bind@epam.com>
2023-04-04 11:11:49 +0530

    [TASK-7] Error handling for basic auth

--- src/components/pages/admin/PageProductImport/PageProductImport.tsx
+++ src/components/pages/admin/PageProductImport/PageProductImport.tsx
@@ -3,15 +3,32 @@ import ProductsTable from "~/components/pages/admin/PageProductImport/components
 import CSVFileImport from "~/components/pages/admin/PageProductImport/components/CSVFileImport";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import AlertTitle from '@mui/material/AlertTitle';
 import { Link } from "react-router-dom";
+import { useCallback, useState } from "react";
 
-export default function PageProductImport() {
+export default function PageProductImport() {  
+
+  const [error, setError] = useState<any>(null);
+
+  const setErrorCB = useCallback((err:any) => {
+    setError(err)
+  }, []);
+
   return (
     <Box py={3}>
+      {error ? (
+      <Alert severity="error">
+        <AlertTitle>Error</AlertTitle>
+        {error?.text}
+      </Alert>
+      ): ''}
       <Box mb={2} display="flex" justifyContent="space-between">
         <CSVFileImport
           url={`${API_PATHS.import}/import`}
           title="Import Products CSV"
+          setErrorCB={setErrorCB}
         />
         <Button
           size="small"
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-east-1.amazonaws.com/dev",
-  import: "https://cwbboke30k.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://9alomzp9sb.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://tacex7h7eh.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-east-1.amazonaws.com/dev",
 };
